3f17b0723d05b08fa584097277b358deaa4041ee
gvjacob <gvjacob@outlook.com>
2020-07-06 20:35:15 -0400

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>papelitos | pieces of paper</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>papelitos</h1>
+      </main>
+    </div>
+  );
+}
