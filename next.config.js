08ea8ef5f6bda014bce5ef2aaed11fbcb8a00c57
gvjacob <gvjacob@outlook.com>
2020-07-07 09:24:54 -0400

    injectSassPrependData lib

--- next.config.js
+++ next.config.js
@@ -1,43 +1,11 @@
 const path = require('path');
 
 function fromSource(to) {
-  const srcDir = path.resolve(__dirname, 'src');
-  return path.resolve(srcDir, to);
+  return path.resolve(__dirname, 'src', to);
 }
 
 module.exports = {
-  webpack: (config) => {
-    let rule, moduleRules, cssLoader, scssRules, sassLoader;
-    if (
-      (rule = config.module.rules.find((rule) =>
-        Object.keys(rule).includes('oneOf'),
-      ))
-    ) {
-      // Locate sass-loader config
-      if (
-        (scssRules = rule.oneOf.filter(
-          (r) =>
-            ('test.scss'.match(r.test) || 'test.module.scss'.match(r.test)) &&
-            Array.isArray(r.use),
-        ))
-      ) {
-        for (const scssRule of scssRules) {
-          if (
-            (sassLoader = scssRule.use.find((u) =>
-              u.loader.match('sass-loader'),
-            ))
-          ) {
-            sassLoader.options = {
-              ...sassLoader.options,
-              // Your custom sass-loader options below.
-              prependData: `@import 'abstracts/index';`,
-            };
-          }
-        }
-      }
-    }
-    return config;
-  },
+  webpack: require('./lib/injectSassPrependData'),
   sassOptions: {
     includePaths: [fromSource('styles')],
   },
