2a5e3326c48211dfa839e775a4bfe6ef3130bfb0
gvjacob <gvjacob@outlook.com>
2020-07-14 15:51:39 -0400

    Use environment variables for firebase

--- src/firebase.js
+++ src/firebase.js
@@ -2,18 +2,13 @@ import * as firebase from 'firebase';
 import 'firebase/firestore';
 
 const config = {
-  apiKey: 'AIzaSyC7PRQWWXLBFu6UCexl1y3EomH3W-jHjKk',
-  authDomain: 'papelitos-9c6ef.firebaseapp.com',
-  databaseURL: 'https://papelitos-9c6ef.firebaseio.com',
-  projectId: 'papelitos-9c6ef',
-  storageBucket: 'papelitos-9c6ef.appspot.com',
-  messagingSenderId: '509368876679',
-  appId: '1:509368876679:web:8a3b5f2c3c8a033db80222',
-  measurementId: 'G-S0N1NGPLSF',
+  apiKey: process.env.FIREBASE_API_KEY,
+  appId: process.env.FIREBASE_APP_ID,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
 };
 
 // Initialize or provide existing app
 export default firebase.apps.length
   ? firebase.app()
   : firebase.initializeApp(config);
-
